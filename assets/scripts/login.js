e73ccc608693c2734ba47acdabb419cc33be52bd
Vitor Hugo <hugo.reisj@hotmail.com>
2025-07-10 00:41:25 -0300

    Integração com backend

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const senhaInput = document.getElementById("senha");
     const hideCheckbox = document.getElementById("hideButton");
+    const loginForm = document.getElementById("form-login");
 
     hideCheckbox.addEventListener("change", () => {
         if (hideCheckbox.checked) {
@@ -9,4 +10,45 @@ document.addEventListener("DOMContentLoaded", () => {
             senhaInput.type = "password";
         }
     });
+
+    loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const senha = document.getElementById("senha").value.trim();
+
+    console.log("Tentando login com:", email, senha);
+
+    try {
+        const response = await fetch("https://oba-dogs-api.onrender.com/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: email,
+                password: senha
+            })
+        });
+
+        console.log("Response status:", response.status);
+
+        if (!response.ok) {
+            alert("Login inválido!");
+            return;
+        }
+
+        const data = await response.json();
+        console.log("Dados recebidos:", data);
+
+        localStorage.setItem("token", data.token);
+
+        // Redirecionar para o painel
+        window.location.href = "./painel-animais.html";
+    } catch (err) {
+        console.error("Erro ao fazer login:", err);
+        alert("Erro ao tentar fazer login. Tente novamente mais tarde.");
+    }
+});
+
 });
